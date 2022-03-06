087c42ff2769b3ac4a14010f8679dda057cd24b4
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-03-06 18:11:19 -0300

    Server created

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+
+const routes = Router()
+
+routes.get('/products', (request, response) => {
+  return response.send({message: "Connect with success"})
+})
+
+export { routes }
