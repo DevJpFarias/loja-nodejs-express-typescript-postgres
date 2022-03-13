0c009354078dcc7b6d2c97edd5a55ec4acf452fd
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-03-12 22:35:11 -0300

    Cleaning

--- src/modules/products/services/DeleteProductService.spec.ts
+++ src/modules/products/services/DeleteProductService.spec.ts
@@ -1,18 +1,15 @@
 import { FakeProductsRepository } from '../repositories/fakes/FakeProductsRepository'
 import { CreateProductsService } from './CreateProductService'
 import { DeleteProductService } from './DeleteProductService'
-import { ListProductsService } from './ListProductsService'
 
 let fakeProductsRepository: FakeProductsRepository
 let createProductService: CreateProductsService
-let listProductService: ListProductsService
 let deleteProductService: DeleteProductService
 
 describe('Delete Product', () => {
 	beforeEach(() => {
 		fakeProductsRepository = new FakeProductsRepository()
 		createProductService = new CreateProductsService(fakeProductsRepository)
-		listProductService = new ListProductsService(fakeProductsRepository)
 		deleteProductService = new DeleteProductService(fakeProductsRepository)
 	})
 
@@ -24,7 +21,7 @@ describe('Delete Product', () => {
 		})
 
 		expect(fakeProductsRepository.products).toEqual([product])
-		
+
 		const product_id = product.id
 
 		await deleteProductService.execute(product_id)
