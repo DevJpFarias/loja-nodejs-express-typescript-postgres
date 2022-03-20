6537f829bfc9a010de9f6f5f3b78170e679ae992
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-03-20 19:17:35 -0300

    UpdateUser test updated

--- src/modules/users/services/UpdateUserService.spec.ts
+++ src/modules/users/services/UpdateUserService.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from '../../../shared/errors/AppError'
 import { FakeUsersRepository } from '../repositories/fakes/FakeUsersRepository'
 import { CreateUserService } from './CreateUserService'
 import { UpdateUserService } from './UpdateUserService'
@@ -31,5 +32,39 @@ describe('User update', () => {
 		expect(user.name).toBe(update_user.name)
 		expect(user.email).toBe(update_user.email)
 		expect(user.password).toBe(update_user.password)
+	})
+
+	it('Should not be able to update an nonexistent user', () => {
+		expect(async () => {
+			await updateUserService.execute({
+				id: '1234',
+				name: 'Paulinho',
+				email: 'joaopaulo@gmail.com',
+				password: '1234'
+			})
+		}).rejects.toBeInstanceOf(AppError)
+	})
+
+	it('Should not be able to update an user with an existent email', () => {
+		expect(async () => {
+			const user = await createUserService.execute({
+				name: 'João Paulo',
+				email: 'joaopaulo@gmail.com',
+				password: '1234'
+			})
+
+			await createUserService.execute({
+				name: 'Isabela',
+				email: 'isaisa@gmail.com',
+				password: 'isabela'
+			})
+
+			await updateUserService.execute({
+				id: user.id,
+				name: 'Paulinho',
+				email: 'isaisa@gmail.com',
+				password: '1234'
+			})
+		}).rejects.toBeInstanceOf(AppError)
 	})
 })
