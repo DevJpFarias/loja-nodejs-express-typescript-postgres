97092ccfc53ca1ff4f9073d37ff634d4558be4f5
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-03-31 00:23:14 -0300

    Updating database connection and creating authentication controller test

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,40 @@
+import 'reflect-metadata'
+import 'express-async-errors'
+import 'es6-shim'
+import express, { NextFunction, Request, Response } from 'express'
+import cors from 'cors'
+import { routes } from './routes'
+import { errors } from 'celebrate'
+import { AppError } from '../../errors/AppError'
+
+import '../../container'
+import '../typeorm'
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(routes)
+
+app.use(errors())
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+	if(err instanceof AppError) {
+		return response.status(err.statusCode).json({
+			status: 'error',
+			message: err.message,
+		})
+	}
+
+	console.log(err)
+
+	return response.status(500).json({
+		status: 'error',
+		message: 'Internal Server Error',
+	})
+})
+
+export { app }
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,13 @@
-import { createConnections } from 'typeorm'
+import { Connection, createConnection, getConnectionOptions } from 'typeorm'
 
-createConnections()
+export default async (host = 'database'): Promise<Connection> => {
+	const defaultOptions = getConnectionOptions()
+
+	return createConnection(
+		await Object.assign(defaultOptions, {
+			host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+			database:
+        process.env.NODE_ENV === 'test' ? 'loja-migufe-test' : (await defaultOptions).database,
+		})
+	)
+}
