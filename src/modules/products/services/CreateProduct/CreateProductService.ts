2c305acc4e56801998746b6d3ec4917a567d7cc0
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-08-15 12:12:47 -0300

    refactor: update CreateProductService

--- src/modules/products/services/CreateProduct/CreateProductService.ts
+++ src/modules/products/services/CreateProduct/CreateProductService.ts
@@ -4,6 +4,7 @@ import { inject, injectable } from 'tsyringe'
 import { Product } from '../../infra/typeorm/entities/Product'
 import { IProductsRepository } from '../../repositories/IProductsRepository'
 import { ICreateProductDTO} from '../../dtos/ICreateProductDTO'
+import { AppError } from '../../../../shared/errors/AppError'
 
 @injectable()
 export class CreateProductsService {
@@ -17,6 +18,10 @@ export class CreateProductsService {
 		description,
 		price
 	}: ICreateProductDTO): Promise<Product> {
+		if(!name) throw new AppError('The product must have a name', 400)
+
+		if(price <= 0) throw new AppError('Cannot have a negative or zero value!', 400)
+
 		const product = await this.productsRepository.create({
 			name,
 			description,
