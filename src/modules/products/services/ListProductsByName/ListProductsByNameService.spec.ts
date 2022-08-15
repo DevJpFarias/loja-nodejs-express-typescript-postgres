c671176254418867701621721caf21f235a308dc
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-08-15 14:59:33 -0300

    refactor: update ListProductByNameService test

--- src/modules/products/services/ListProductsByName/ListProductsByNameService.spec.ts
+++ src/modules/products/services/ListProductsByName/ListProductsByNameService.spec.ts
@@ -1,3 +1,4 @@
+import { ICreateProductDTO } from '../../dtos/ICreateProductDTO'
 import { FakeProductsRepository } from '../../repositories/fakes/FakeProductsRepository'
 import { CreateProductsService } from '../CreateProduct/CreateProductService'
 import { ListProductsByNameService } from './ListProductsByNameService'
@@ -14,16 +15,20 @@ describe('List Products', () => {
 	})
 
 	it('Should be able to list a array of products with his names', async () => {
-		const product = await createProductsService.execute({
+		const product: ICreateProductDTO = await createProductsService.execute({
 			name: 'Sorvete',
 			description: 'Sabor flocos',
-			price: 20
+			price: 20,
+			brand: 'Brand',
+			expiration_date: new Date(2022, 12, 31)
 		})
 
-		const product2 = await createProductsService.execute({
+		const product2: ICreateProductDTO = await createProductsService.execute({
 			name: 'Sorvete',
 			description: 'Sabor chocolate',
-			price: 25
+			price: 25,
+			brand: 'Brand',
+			expiration_date: new Date(2022, 12, 31)
 		})
 
 		const products = await listProductsByNameService.execute('Sorvete')
