c45e96e7118240d68c6bb81f9f49fd76f742d732
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-08-15 12:13:01 -0300

    refactor: update CreateProductService test

--- src/modules/products/services/CreateProduct/CreateProductService.spec.ts
+++ src/modules/products/services/CreateProduct/CreateProductService.spec.ts
@@ -1,3 +1,5 @@
+import { AppError } from '../../../../shared/errors/AppError'
+import { ICreateProductDTO } from '../../dtos/ICreateProductDTO'
 import  { FakeProductsRepository } from '../../repositories/fakes/FakeProductsRepository'
 import { CreateProductsService } from './CreateProductService'
 
@@ -8,6 +10,36 @@ describe('Products creation', () => {
 	beforeEach(() => {
 		fakeProductsRepository = new FakeProductsRepository()
 		createProductsService = new CreateProductsService(fakeProductsRepository)
+	})
+
+	it('Should not be able to create a new product without name', async () => {
+		const data: ICreateProductDTO = {
+			name: '',
+			description: 'Sabor morango',
+			price: 3
+		}
+
+		await expect(createProductsService.execute(data)).rejects.toEqual(new AppError('The product must have a name', 400))
+	})
+
+	it('Should not be able to create a new product zero value', async () => {
+		const data: ICreateProductDTO = {
+			name: 'Biscoito',
+			description: 'Morango',
+			price: 0
+		}
+
+		await expect(createProductsService.execute(data)).rejects.toEqual(new AppError('Cannot have a negative or zero value!', 400))
+	})
+
+	it('Should not be able to create a new product with negative value', async () => {
+		const data: ICreateProductDTO = {
+			name: 'Biscoito',
+			description: 'Sabor morango',
+			price: -3
+		}
+
+		await expect(createProductsService.execute(data)).rejects.toEqual(new AppError('Cannot have a negative or zero value!', 400))
 	})
 
 	it('Should be able to create a new product', async () => {
