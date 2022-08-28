9a3b7c949d021e52ce23840a09c47a897822cc9b
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-08-27 22:48:26 -0300

    refactor: update ListAllProductsController

--- src/modules/products/infra/http/controllers/ListAllProducts/ListAllProductsController.ts
+++ src/modules/products/infra/http/controllers/ListAllProducts/ListAllProductsController.ts
@@ -1,13 +1,14 @@
 import 'reflect-metadata'
 import 'reflect-metadata'
 import { Request, Response } from 'express'
-import { container } from 'tsyringe'
 import { ListAllProductsService } from '../ProductsControllerHelpers'
+import { ProductsRepository } from '../../../typeorm/repositories/ProductsRepository'
 
+const productsRepository = new ProductsRepository()
 
 export class ListAllProductsController {
 	async handle (request: Request, response: Response): Promise<Response> {
-		const listAllProductsService = container.resolve(ListAllProductsService)
+		const listAllProductsService = new ListAllProductsService(productsRepository)
 
 		const products = await listAllProductsService.execute()
 
