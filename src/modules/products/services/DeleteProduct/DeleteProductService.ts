f6dcb5c55a68b2bbaf4aca172813f1f3eb5addbb
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-08-27 22:33:36 -0300

    refactor: update DeleteProductService

--- src/modules/products/services/DeleteProduct/DeleteProductService.ts
+++ src/modules/products/services/DeleteProduct/DeleteProductService.ts
@@ -1,19 +1,23 @@
-import { inject, injectable } from 'tsyringe'
 import { AppError } from '../../../../shared/errors/AppError'
 import { IDeleteProductDTO } from '../../dtos/IDeleteProductDTO'
+import { ProductsRepository } from '../../infra/typeorm/repositories/ProductsRepository'
 import { IProductsRepository } from '../../repositories/IProductsRepository'
 
-@injectable()
 export class DeleteProductService {
-	constructor(
-    @inject('ProductsRepository')
-    private productsRepository: IProductsRepository
-	) {}
+	private productsRepository: IProductsRepository
+
+	constructor(repository: IProductsRepository) {
+		this.productsRepository = repository
+
+		if(!repository) {
+			this.productsRepository = new ProductsRepository()
+		}
+	}
 
 	async execute({ id }: IDeleteProductDTO): Promise<void> {
 		const product = await this.productsRepository.findById(id)
 
-		if(!product) throw new AppError('Product not found!', 404)
+		if(!product) throw new AppError('Product not found!', 400)
 
 		await this.productsRepository.delete(product)
 	}
