540b692b3e3238b69a35c634b06c5a291ed18037
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-08-27 22:47:02 -0300

    refactor: update ListAllProductService

--- src/modules/products/services/ListAllProducts/ListAllProductsService.ts
+++ src/modules/products/services/ListAllProducts/ListAllProductsService.ts
@@ -1,13 +1,17 @@
-import { inject, injectable } from 'tsyringe'
 import { Product } from '../../infra/typeorm/entities/Product'
+import { ProductsRepository } from '../../infra/typeorm/repositories/ProductsRepository'
 import { IProductsRepository } from '../../repositories/IProductsRepository'
 
-@injectable()
 export class ListAllProductsService {
-	constructor(
-    @inject('ProductsRepository')
-    private productsRepository: IProductsRepository
-	) {}
+	private productsRepository: IProductsRepository
+
+	constructor(repository: IProductsRepository) {
+		this.productsRepository = repository
+
+		if(!repository) {
+			this.productsRepository = new ProductsRepository()
+		}
+	}
 
 	async execute(): Promise<Product[]> {
 		const products = await this.productsRepository.listAll()
