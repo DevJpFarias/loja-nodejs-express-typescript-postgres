92c5b505fb57aa9ffeadfc6170698bb5cebdec3e
João Paulo Farias Cabral <joaopfc2764@gmail.com>
2022-08-27 22:31:57 -0300

    refactor: update createProductController

--- src/modules/products/infra/http/controllers/CreateProduct/CreateProductController.ts
+++ src/modules/products/infra/http/controllers/CreateProduct/CreateProductController.ts
@@ -1,13 +1,15 @@
 import 'reflect-metadata'
 import { Request, Response } from 'express'
-import { container } from 'tsyringe'
 import { CreateProductsService } from '../ProductsControllerHelpers'
+import { ProductsRepository } from '../../../typeorm/repositories/ProductsRepository'
+
+const productsRepository = new ProductsRepository()
 
 export class CreateProductController {
 	async handle (request: Request, response: Response): Promise<Response> {
 		const { name, description, price, brand, expiration_date } = request.body
 	
-		const createProductsService = container.resolve(CreateProductsService)
+		const createProductsService = new CreateProductsService(productsRepository)
 	
 		const product = await createProductsService.execute({
 			name,
